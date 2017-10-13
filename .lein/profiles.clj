ec05c3cb3cd23c06ab7db94388edac830d412a90
护群神兽之点赞狂魔 <731014656@qq.com>
2017-10-13 10:26:51 +0800

    Update profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user
   {
   :java-cmd "C:\\Program Files\\Java\\jdk1.8.0_45\\bin\\java.exe"
-  :plugins [[cider/cider-nrepl "0.8.1"]]
+  :plugins [[cider/cider-nrepl "0.8.1"]
+            [lein-gorilla "0.4.0"]]
    }
  }
